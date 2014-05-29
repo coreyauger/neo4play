cbf5c618d3bfa76fc7cb3b1975f0f2e47bbc0231
coreyauger <coreyauger@gmail.com>
2014-05-28 21:52:41 -0700

    * start of working slim non-blocking neo client

--- src/main/scala/Neo4Play.scala
+++ src/main/scala/Neo4Play.scala
@@ -1,32 +1,60 @@
 package com.nxtwv.neo4play
 
-import play.api.libs.json.{Json, JsValue}
 import scala.concurrent.Future
 import play.api.libs.ws.WS
 import play.api.libs.concurrent.Execution.Implicits._
+import play.api.libs.json.{JsObject, JsValue, Json}
 
 trait NeoService{
-  def cypher(implicit s:Neo4JServer, query:String, params:Map[String, String]):Future[JsValue]
+  def cypher(query:String, params:Map[String, String])(implicit s:Neo4JServer):Future[JsValue]
 }
 
 sealed class Neo4JServer(val host:String, val port:Int, val path:String){
   def url(part:String) = {
-    "%s:%s%s/%s".format(host,port,path,part)
+    "http://%s:%s%s%s".format(host,port,path,part)
   }
 }
 
 object Neo4Play extends NeoService{
 
-  def cypher(implicit s:Neo4JServer, query:String, params:Map[String, String]):Future[JsValue] ={
-    WS.url(s.url("cypher")).post(
-      Json.obj(
-        "query" -> query,
-        "params" -> params
-      )
-    ).map{
+  def cypher(query:String, params:Map[String, String])(implicit s:Neo4JServer):Future[JsValue] ={
+    val json = Json.obj(
+      "query" -> query,
+      "params" -> params
+    )
+    println(s.url("cypher"))
+    WS.url(s.url("cypher"))
+      .withHeaders( ("Accept","application/json; charset=UTF-8"), ("Content-Type", "application/json") )
+      .post(
+        json
+      ).map{
       res =>
+        println("RESPONSE")
         res.json
     }
+  }
+
+  def neoJsonToDataObjectList(json:JsValue):List[JsObject] = {
+    println("neoJsonToDataObjectList")
+    //println(json)
+    (json \ "data").apply(0).as[List[JsObject]]
+  }
+  def neoJsonToDataList(json:JsValue):List[List[String]] = {
+    println("neoJsonToDataList")
+    // println(json)
+    (json \ "data").as[List[List[String]]]
+  }
+
+  def neoJsonToDataMap(json:JsValue):List[Map[String,String]] = {
+    println("neoJsonToDataMap")
+    //println(json)
+    val data = (json \ "data").apply(0).as[List[JsObject]]
+    data.map{
+      jobj =>
+        (jobj \ "data").as[JsObject].value.map{
+          case (k,v) => (k,v.as[String])
+        }.toMap
+    }.toList
   }
 
 }
